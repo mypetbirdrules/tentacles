f7d8aefd0c81b7f759371c92bf686b5b3971a91b
Kyle Stankovich <gamemakergml@gmail>
2016-05-26 23:25:28 -0400

    Added SCSS and basic PHP. Site now runs off of phptesting.webuda.com  On branch kyle_branch  Changes to be committed: 	deleted:    about/template.html 	new file:   about/template.php 	new file:   config.php 	new file:   css/main.css 	new file:   css/main.min.css 	new file:   css/main.scss 	new file:   css/mixins.scss 	modified:   index.css 	new file:   index.php 	deleted:    pictures/template.html 	new file:   pictures/template.php 	new file:   templates/header.php 	new file:   templates/template.php 	deleted:    upload/template.html 	new file:   upload/template.php 	deleted:    videos/template.html 	new file:   videos/template.php
     Changes not staged for commit:
    	deleted:    html_template/template.html
    	deleted:    index.html
    	deleted:    main.css

---
+++ about/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Example</title>
+    <link rel="stylesheet" type="text/css" href='../css/main.min.css'>
+  </head>
+
+  <body>
+    <?php include_once('../templates/header.php'); ?>
+
+    <div id="contentArea">
+
+    </div>
+  </body>
+</html>
